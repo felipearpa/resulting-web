0b8f601a8def717d26dcfbe13f854f2398d76628
Felipe Arcila Parra <felipearcila@gmail.com>
2024-11-16 11:05:11 -0500

    Add non-nullable constraint to Result types
    Refactored `Result` types to enforce that values cannot be `null` or `undefined` by introducing the `extends NonNullable<unknown>` constraint. This ensures safer and more reliable type handling, preventing potential runtime errors due to unexpected `null` or `undefined` values. Updated related functions and documentation to reflect this change.

--- src/result-catching.extension.ts
+++ src/result-catching.extension.ts
@@ -13,18 +13,18 @@ declare module './result' {
          * @return {Result<NewValue>} The encapsulated result of the given transform function applied to the encapsulated value if this instance represents
          * success or the original encapsulated error if it is failure.
          */
-        mapCatching<NewValue>(transform: (value: Value) => NewValue): Result<NewValue>;
+        mapCatching<NewValue extends NonNullable<unknown>>(transform: (value: Value) => NewValue): Result<NewValue>;
 
         /**
          * Returns the encapsulated result of the given transform function applied to the encapsulated error if this instance represents failure or the original
-         * encapsulated value if it is success.
+         * encapsulated value if it is a success.
          *
          * @template NewValue
          * @param {(Error) => NewValue} transform - The transformation function to apply to the error.
          * @return {Result<NewValue>} The encapsulated result of the given transform function applied to the encapsulated error if this instance represents
          * failure or the original encapsulated value if it is success.
          */
-        recoverCatching<NewValue>(transform: (error: Error) => NewValue): Result<NewValue>;
+        recoverCatching<NewValue extends NonNullable<unknown>>(transform: (error: Error) => NewValue): Result<NewValue>;
     }
 }
 
@@ -35,7 +35,10 @@ Result.prototype.mapCatching = function (transform) {
     return Result.failure(this.value as Error);
 };
 
-Result.prototype.recoverCatching = function <NewValue, value extends NewValue>(this: Result<value>, transform: (error: Error) => NewValue) {
+Result.prototype.recoverCatching = function <NewValue extends NonNullable<unknown>, Value extends NewValue>(
+    this: Result<Value>,
+    transform: (error: Error) => NewValue,
+) {
     const error = this.errorOrNull();
     if (error == null) return this;
     return runCatching(() => transform(error));
